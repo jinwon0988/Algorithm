1ffc761fb4e2f41f8114732ac6f37114d081a6b6
Jinwon Kim <104982917+jinwon0988@users.noreply.github.com>
2025-04-24 23:21:46 +0900

    [Gold III] Title: 나머지 합, Time: 464 ms, Memory: 120676 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		long[] S = new long[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+		long res = 0;
+		int remainder;
+		long [] C = new long[M];
+		for (int i = 1; i <= N; i++) {
+			remainder = (int) (S[i] % M);
+			if (remainder == 0) {
+				res++;
+			}
+			C[remainder]++;
+		}
+
+		for (int i = 0; i < M; i++) {
+			if (C[i] > 1) {
+				res += C[i] * (C[i] - 1) / 2;
+			}
+		}
+
+		System.out.println(res);
+	}
+}
