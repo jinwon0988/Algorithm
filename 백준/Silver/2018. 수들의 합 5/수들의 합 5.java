f42fbcfd07ede71bca13053cb91e08faaf115178
Jinwon Kim <104982917+jinwon0988@users.noreply.github.com>
2025-05-08 03:36:11 +0900

    [Silver V] Title: 수들의 합 5, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 1, start_index = 1, end_index = 1, sum = 1;
+
+		int N = sc.nextInt();
+
+		while (end_index != N) {
+			if (sum == N) {
+				count++;
+				end_index++;
+				sum += end_index;
+			} else if (sum > N) {
+				sum -= start_index;
+				start_index++;
+			} else {
+				end_index++;
+				sum += end_index;
+			}
+		}
+
+		System.out.println(count);
+
+		sc.close();
+	}
+
+}
