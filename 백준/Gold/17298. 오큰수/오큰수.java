60710fee5ffea0f16a9df3c7210a2d1e8fc4e511
Jinwon Kim <104982917+jinwon0988@users.noreply.github.com>
2025-05-16 16:31:51 +0900

    [Gold IV] Title: 오큰수, Time: 960 ms, Memory: 151300 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -8,21 +8,21 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] A = new int[N];
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
 		Stack<Integer> stack = new Stack<>();
 		int[] ans = new int[N];
-		for(int i=0; i<N; i++) {
-			while(!stack.isEmpty() && A[stack.peek()] < A[i]) {
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
 				ans[stack.pop()] = A[i];
 			}
 			stack.push(i);
- 		}
-		while(!stack.isEmpty()) {
+		}
+		while (!stack.isEmpty()) {
 			ans[stack.pop()] = -1;
 		}
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			bw.write(ans[i] + " ");
 		}
 		bw.flush();
