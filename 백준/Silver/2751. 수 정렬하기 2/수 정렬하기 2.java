87f1629fa82ac9de8e90d47af714182edd29e688
Jinwon Kim <104982917+jinwon0988@users.noreply.github.com>
2025-05-17 20:48:26 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1004 ms, Memory: 203076 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		mergeSort(A, 0, N - 1);
+		for (int i = 0; i < N; i++) {
+			bw.write(A[i] + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+	private static void mergeSort(int[] A, int left, int right) {
+		if (left < right) {
+			int mid = (left + right) / 2;
+			mergeSort(A, left, mid);
+			mergeSort(A, mid + 1, right);
+			merge(A, left, mid, right);
+		}
+	}
+
+	private static void merge(int[] A, int left, int mid, int right) {
+		int[] temp = new int[right - left + 1];
+		int i = left;
+		int j = mid + 1;
+		int k = 0;
+
+		while (i <= mid && j <= right) {
+			if (A[i] <= A[j]) {
+				temp[k++] = A[i++];
+			} else {
+				temp[k++] = A[j++];
+			}
+		}
+
+		while (i <= mid) {
+			temp[k++] = A[i++];
+		}
+
+		while (j <= right) {
+			temp[k++] = A[j++];
+		}
+
+		for (int t = 0; t < temp.length; t++) {
+			A[left + t] = temp[t];
+		}
+	}
+}
