a1aa4488e94feced24744b9a34729ad18d52a32f
Jinwon Kim <jinwon0988@naver.com>
2025-05-28 22:29:11 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 16132 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+		bw.write(A / B + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
