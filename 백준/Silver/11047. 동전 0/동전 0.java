a9184851c9eee8469fb779573b69bca0273aaf43
Jinwon Kim <jinwon0988@naver.com>
2025-05-31 15:57:00 +0900

    [Silver IV] Title: 동전 0, Time: 116 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		int count = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			if (A[i] <= K) {
+				count += (K / A[i]);
+				K = K % A[i];
+			}
+		}
+		bw.write(count + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
