44592c89a115379e2cbd382c386ee83de04a4a6b
Jinwon Kim <jinwon0988@naver.com>
2025-06-01 15:49:36 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 14988 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int result = a * b / gcd(a, b);
+			bw.write(result + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+	public static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+}
