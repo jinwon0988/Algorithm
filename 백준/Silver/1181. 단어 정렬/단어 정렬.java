9f403deff27e1afd2108634e8003b3e16b871523
Jinwon Kim <jinwon0988@naver.com>
2025-06-03 20:34:07 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 23872 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		String[] str = new String[N];
+		for (int i = 0; i < N; i++) {
+			str[i] = br.readLine();
+		}
+		Arrays.sort(str, new Comparator<>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.length() == o2.length()) {
+					return o1.compareTo(o2);
+				} else {
+					return o1.length() - o2.length();
+				}
+			}
+
+		});
+		bw.write(str[0] + "\n");
+		for (int i = 1; i < N; i++) {
+			if (!str[i].equals(str[i - 1])) {
+				bw.write(str[i] + "\n");
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+}
