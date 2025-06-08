7127642dd30728d1ab30199ed3756fdf2607ce6e
Jinwon Kim <jinwon0988@naver.com>
2025-06-08 22:10:32 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,54 +1,64 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int[][] tree;
-  public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    sc.nextLine();
-    tree = new int[26][2];
-    for (int i = 0; i < n; i++) {
-      String[] temp = sc.nextLine().split(" ");
-      int node = temp[0].charAt(0) - 'A';
-      char left = temp[1].charAt(0);
-      char right = temp[2].charAt(0);
-      if (left == '.') {
-        tree[node][0] = -1;
-      } else {
-        tree[node][0] = left - 'A';
-      }
-      if (right == '.') {
-        tree[node][1] = -1;
-      } else {
-        tree[node][1] = right - 'A';
-      }
-    }
-    preOrder(0);
-    System.out.println();
-    inOrder(0);
-    System.out.println();
-    postOrder(0);
-    System.out.println();
-  }
-  public static void preOrder(int now) {
-    if (now == -1)
-      return;
-    System.out.print((char) (now + 'A'));
-    preOrder(tree[now][0]);
-    preOrder(tree[now][1]);
-  }
-  public static void inOrder(int now) {
-    if (now == -1)
-      return;
-    inOrder(tree[now][0]);
-    System.out.print((char) (now + 'A'));
-    inOrder(tree[now][1]);
-  }
-  public static void postOrder(int now) {
-    if (now == -1)
-      return;
-    postOrder(tree[now][0]);
-    postOrder(tree[now][1]);
-    System.out.print((char) (now + 'A'));
-  }
+	static int[][] tree;
+	static BufferedWriter bw;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		tree = new int[26][2];
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int node = st.nextToken().charAt(0) - 'A';
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+			if (left == '.') {
+				tree[node][0] = -1;
+			} else {
+				tree[node][0] = left - 'A';
+			}
+			if (right == '.') {
+				tree[node][1] = -1;
+			} else {
+				tree[node][1] = right - 'A';
+			}
+		}
+		preOrder(0);
+		bw.write("\n");
+		inOrder(0);
+		bw.write("\n");
+		postOrder(0);
+		bw.write("\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	public static void preOrder(int now) throws Exception {
+		if (now == -1)
+			return;
+		bw.write((char) (now + 'A'));
+		preOrder(tree[now][0]);
+		preOrder(tree[now][1]);
+	}
+
+	public static void inOrder(int now) throws Exception {
+		if (now == -1)
+			return;
+		inOrder(tree[now][0]);
+		bw.write((char) (now + 'A'));
+		inOrder(tree[now][1]);
+	}
+
+	public static void postOrder(int now) throws Exception {
+		if (now == -1)
+			return;
+		postOrder(tree[now][0]);
+		postOrder(tree[now][1]);
+		bw.write((char) (now + 'A'));
+	}
 }
