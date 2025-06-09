6e8d42af051318c1eb1de1ea1ca65e7000da8fc5
Jinwon Kim <jinwon0988@naver.com>
2025-06-09 16:54:30 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 140 ms, Memory: 16172 KB -BaekjoonHub

---
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int M = Integer.parseInt(br.readLine());
+		int[] color = new int[M + 1];
+		int N = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= M; i++) {
+			color[i] = Integer.parseInt(st.nextToken());
+			N += color[i];
+		}
+		int K = Integer.parseInt(br.readLine());
+		double result = 0.0;
+		for (int i = 1; i <= M; i++) {
+			double temp = 1.0;
+			for (int j = 0; j < K; j++) {
+				temp *= (color[i] - j) / (double) (N - j);
+			}
+			result += temp;
+		}
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
