c3bae9b34bce60a755c9348474cb00bb936ff8a9
Jinwon Kim <jinwon0988@naver.com>
2025-06-10 17:06:06 +0900

    [Silver III] Title: 퇴사, Time: 120 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[] D = new int[N + 2];
+		int[] T = new int[N + 1];
+		int[] P = new int[N + 1];
+		StringTokenizer st;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = N; i > 0; i--) {
+			if (i + T[i] - 1 > N) {
+				D[i] = D[i + 1];
+			} else {
+				D[i] = Math.max(D[i + 1], P[i] + D[i + T[i]]);
+			}
+		}
+		bw.write(D[1] + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
