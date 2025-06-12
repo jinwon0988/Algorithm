62fc139a6dc31dc9cbe02a883e68f8a97af19afd
Jinwon Kim <jinwon0988@naver.com>
2025-06-12 13:35:32 +0900

    [Silver III] Title: 이친수, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,16 +1,20 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    long D[][] = new long[N+1][2];
-    D[1][1] = 1;
-    D[1][0] = 0;
-    for(int i=2; i<=N; i++){
-      D[i][0] = D[i-1][1] + D[i-1][0]; 
-      D[i][1] = D[i-1][0]; 
-    }
-    System.out.println(D[N][0] + D[N][1]);
-  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		long[][] D = new long[N + 1][2];
+		D[1][1] = 1;
+		D[1][0] = 0;
+		for (int i = 2; i <= N; i++) {
+			D[i][0] = D[i - 1][0] + D[i - 1][1];
+			D[i][1] = D[i - 1][0];
+		}
+		bw.write(D[N][0] + D[N][1] + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
