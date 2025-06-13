a01842d7d7e9f556e2411bb488c5781d744c2160
Jinwon Kim <jinwon0988@naver.com>
2025-06-13 15:53:30 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
-		int sum = 0, max = 0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] scores = new int[N];
 		for (int i = 0; i < N; i++) {
-			int avg = sc.nextInt();
-			sum += avg;
-			if (max < avg) {
-				max = avg;
-			}
+			scores[i] = Integer.parseInt(st.nextToken());
 		}
-
-		System.out.println(sum * 100.0 / max / N);
+		int max = scores[0];
+		for (int i = 1; i < N; i++) {
+			max = Math.max(max, scores[i]);
+		}
+		double sum = 0.0;
+		for (int i = 0; i < N; i++) {
+			sum += (double) scores[i] / (double) max * 100.0;
+		}
+		double avg = sum / N;
+		bw.write(avg + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
 }
