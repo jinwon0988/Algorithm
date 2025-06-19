a709d02d9b1955db92312adc24a7df6bf1adc8ee
Jinwon Kim <jinwon0988@naver.com>
2025-06-19 13:52:49 +0900

    [Gold V] Title: CCW, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		int result = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3);
+		if (result > 0)
+			bw.write("1\n");
+		else if (result < 0)
+			bw.write("-1\n");
+		else
+			bw.write("0\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
