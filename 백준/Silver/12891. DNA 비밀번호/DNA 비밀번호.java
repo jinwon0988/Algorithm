445ac2cc2c63d269769f601b7cf3b29718660b43
Jinwon Kim <jinwon0988@naver.com>
2025-06-21 16:25:37 +0900

    [Silver II] Title: DNA 비밀번호, Time: 288 ms, Memory: 21708 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,90 +2,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] myArr;
-	static int[] checkArr;
-	static int checkSecret;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int S = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
-		char[] A = new char[S];
-		A = br.readLine().toCharArray();
-		checkArr = new int[4];
+		String DNA = br.readLine();
+		int[] check = new int[4];
 		st = new StringTokenizer(br.readLine());
-		checkSecret = 0;
 		for (int i = 0; i < 4; i++) {
-			checkArr[i] = Integer.parseInt(st.nextToken());
-			if (checkArr[i] == 0)
-				checkSecret++;
+			check[i] = Integer.parseInt(st.nextToken());
 		}
-		myArr = new int[4];
+		int[] state = new int[4];
 		for (int i = 0; i < P; i++) {
-			Add(A[i]);
+			char ch = DNA.charAt(i);
+			switch (ch) {
+			case 'A':
+				state[0]++;
+				break;
+			case 'C':
+				state[1]++;
+				break;
+			case 'G':
+				state[2]++;
+				break;
+			case 'T':
+				state[3]++;
+				break;
+			}
 		}
 		int count = 0;
-		if (checkSecret == 4)
+		if (state[0] >= check[0] && state[1] >= check[1] && state[2] >= check[2] && state[3] >= check[3]) {
 			count++;
-		for (int j = P; j < S; j++) {
-			int i = j - P;
-			Add(A[j]);
-			Remove(A[i]);
-			if (checkSecret == 4)
+		}
+		for (int i = 0; i < S - P; i++) {
+			int e = i + P;
+			char ch1 = DNA.charAt(i);
+			switch (ch1) {
+			case 'A':
+				state[0]--;
+				break;
+			case 'C':
+				state[1]--;
+				break;
+			case 'G':
+				state[2]--;
+				break;
+			case 'T':
+				state[3]--;
+				break;
+			}
+			char ch2 = DNA.charAt(e);
+			switch (ch2) {
+			case 'A':
+				state[0]++;
+				break;
+			case 'C':
+				state[1]++;
+				break;
+			case 'G':
+				state[2]++;
+				break;
+			case 'T':
+				state[3]++;
+				break;
+			}
+			if (state[0] >= check[0] && state[1] >= check[1] && state[2] >= check[2] && state[3] >= check[3]) {
 				count++;
+			}
 		}
-		System.out.println(count);
+		bw.write(count + "\n");
+		bw.flush();
+		bw.close();
 		br.close();
-	}
-
-	private static void Add(char c) {
-		switch (c) {
-		case 'A':
-			myArr[0]++;
-			if (myArr[0] == checkArr[0])
-				checkSecret++;
-			break;
-		case 'C':
-			myArr[1]++;
-			if (myArr[1] == checkArr[1])
-				checkSecret++;
-			break;
-		case 'G':
-			myArr[2]++;
-			if (myArr[2] == checkArr[2])
-				checkSecret++;
-			break;
-		case 'T':
-			myArr[3]++;
-			if (myArr[3] == checkArr[3])
-				checkSecret++;
-			break;
-		}
-	}
-
-	private static void Remove(char c) {
-		switch (c) {
-		case 'A':
-			if (myArr[0] == checkArr[0])
-				checkSecret--;
-			myArr[0]--;
-			break;
-		case 'C':
-			if (myArr[1] == checkArr[1])
-				checkSecret--;
-			myArr[1]--;
-			break;
-		case 'G':
-			if (myArr[2] == checkArr[2])
-				checkSecret--;
-			myArr[2]--;
-			break;
-		case 'T':
-			if (myArr[3] == checkArr[3])
-				checkSecret--;
-			myArr[3]--;
-			break;
-		}
 	}
 }
