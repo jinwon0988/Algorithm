8123115d2b23b13b1f46ebe8130ed283e974fc2d
Jinwon Kim <jinwon0988@naver.com>
2025-06-22 16:54:02 +0900

    [Gold IV] Title: 오큰수, Time: 920 ms, Memory: 150056 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,30 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
+		int[] answer = new int[N];
 		Stack<Integer> stack = new Stack<>();
-		int[] ans = new int[N];
 		for (int i = 0; i < N; i++) {
 			while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
-				ans[stack.pop()] = A[i];
+				answer[stack.pop()] = A[i];
 			}
 			stack.push(i);
 		}
 		while (!stack.isEmpty()) {
-			ans[stack.pop()] = -1;
+			answer[stack.pop()] = -1;
 		}
 		for (int i = 0; i < N; i++) {
-			bw.write(ans[i] + " ");
+			bw.write(answer[i] + " ");
 		}
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
