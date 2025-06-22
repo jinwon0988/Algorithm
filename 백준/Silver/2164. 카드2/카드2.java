893f1963823fa50de44ef53ff3d0e9dc7f2b5693
Jinwon Kim <jinwon0988@naver.com>
2025-06-22 17:02:37 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 45800 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,17 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
 		Queue<Integer> queue = new LinkedList<>();
-		int N = sc.nextInt();
 		for (int i = 1; i <= N; i++) {
 			queue.add(i);
 		}
-		while (queue.size() > 1) {
+		for (int i = 0; i < N - 1; i++) {
 			queue.poll();
 			queue.add(queue.poll());
 		}
-		System.out.println(queue.poll());
+		bw.write(queue.peek() + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
