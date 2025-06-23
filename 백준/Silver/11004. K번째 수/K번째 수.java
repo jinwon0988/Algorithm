3e6d80fd19c56b7998e6ac83e3af1283369789e3
Jinwon Kim <jinwon0988@naver.com>
2025-06-23 16:23:16 +0900

    [Silver V] Title: K번째 수, Time: 1080 ms, Memory: 368756 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,20 +17,23 @@ public class Main {
 		bw.write(A[K - 1] + "\n");
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 
-	private static void quickSort(int[] A, int S, int E, int K) {
-		int pivot = partition(A, S, E);
-		if (pivot == K) {
-			return;
-		} else if (pivot > K) {
-			quickSort(A, S, pivot - 1, K);
-		} else {
-			quickSort(A, pivot + 1, E, K);
+	public static void quickSort(int[] A, int S, int E, int K) {
+		if (S < E) {
+			int pivot = partition(A, S, E);
+			if (pivot == K) {
+				return;
+			} else if (K < pivot) {
+				quickSort(A, S, pivot - 1, K);
+			} else if (K > pivot) {
+				quickSort(A, pivot + 1, E, K);
+			}
 		}
 	}
 
-	private static int partition(int[] A, int S, int E) {
+	public static int partition(int[] A, int S, int E) {
 		if (S + 1 == E) {
 			if (A[S] > A[E])
 				swap(A, S, E);
@@ -39,24 +42,28 @@ public class Main {
 		int M = (S + E) / 2;
 		swap(A, S, M);
 		int pivot = A[S];
-		int i = S + 1;
-		int j = E;
+		int i = S + 1, j = E;
 		while (i <= j) {
-			while (pivot < A[j] && j > 0)
+			while (j >= S + 1 && pivot < A[j]) {
 				j--;
-			while (pivot > A[i] && i < A.length - 1)
+			}
+			while (i <= E && pivot > A[i]) {
 				i++;
-			if (i <= j)
+			}
+			if (i < j) {
 				swap(A, i++, j--);
+			} else {
+				break;
+			}
 		}
 		A[S] = A[j];
 		A[j] = pivot;
 		return j;
 	}
 
-	private static void swap(int[] A, int S, int E) {
-		int temp = A[S];
-		A[S] = A[E];
-		A[E] = temp;
+	public static void swap(int[] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
 	}
 }
