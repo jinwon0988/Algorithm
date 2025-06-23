4ec370f9b6d31f393285bd32d0a40bab0b47ee21
Jinwon Kim <jinwon0988@naver.com>
2025-06-23 15:10:56 +0900

    [Gold II] Title: 버블 소트, Time: 1444 ms, Memory: 75060 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,37 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-class Node implements Comparable<Node> {
-	int num;
-	int idx;
-
-	Node(int num, int idx) {
-		this.num = num;
-		this.idx = idx;
-	}
-
-	@Override
-	public int compareTo(Node o) {
-		return num - o.num;
-	}
-}
-
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		Node[] A = new Node[N];
+		Data[] A = new Data[N];
 		for (int i = 0; i < N; i++) {
-			A[i] = new Node(Integer.parseInt(br.readLine()), i);
+			A[i] = new Data(i, Integer.parseInt(br.readLine()));
 		}
-		Arrays.sort(A);
+		Arrays.sort(A, (o1, o2) -> Integer.compare(o1.value, o2.value));
 		int max = 0;
 		for (int i = 0; i < N; i++) {
-			max = Math.max(max, A[i].idx - i);
+			if (max < A[i].index - i) {
+				max = A[i].index - i;
+			}
 		}
 		bw.write((max + 1) + "\n");
 		bw.flush();
 		bw.close();
+		br.close();
+	}
+
+	static class Data {
+		int index;
+		int value;
+
+		Data(int index, int value) {
+			this.index = index;
+			this.value = value;
+		}
 	}
 }
