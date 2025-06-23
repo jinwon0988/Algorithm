385bcb5d6964dfacab8feafbf7df28bf5ba0ed27
Jinwon Kim <jinwon0988@naver.com>
2025-06-24 01:16:33 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = i;
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			while (s < e) {
+				int temp = A[s];
+				A[s] = A[e];
+				A[e] = temp;
+				s++;
+				e--;
+			}
+		}
+		for (int i = 1; i <= N; i++) {
+			bw.write(A[i] + " ");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
