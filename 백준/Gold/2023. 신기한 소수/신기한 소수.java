c229051846cc2cccfb3a03e75024ac0674f12008
Jinwon Kim <jinwon0988@naver.com>
2025-06-24 15:30:28 +0900

    [Gold V] Title: 신기한 소수, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -28,7 +28,7 @@ public class Main {
 	}
 
 	static boolean isPrime(int num) {
-		for (int i = 2; i <= Math.sqrt(num); i++) {
+		for (int i = 2; i * i <= num; i++) {
 			if (num % i == 0)
 				return false;
 		}
