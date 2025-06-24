a465de42609d80e3f225d6c716eae88bc436217c
Jinwon Kim <jinwon0988@naver.com>
2025-06-24 15:28:28 +0900

    [Gold V] Title: 신기한 소수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,36 +1,35 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	static int N;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		DFS(2, 1);
 		DFS(3, 1);
 		DFS(5, 1);
 		DFS(7, 1);
+		br.close();
 	}
 
-	private static void DFS(int number, int digit) {
-		if (digit == N) {
+	static void DFS(int number, int jarisu) {
+		if (jarisu == N) {
 			if (isPrime(number)) {
 				System.out.println(number);
 			}
+			return;
 		}
-		for (int i = 1; i < 10; i++) {
-			if (i % 2 == 0)
-				continue;
+		for (int i = 1; i < 10; i += 2) {
 			if (isPrime(number * 10 + i)) {
-				DFS(number * 10 + i, digit + 1);
+				DFS(number * 10 + i, jarisu + 1);
 			}
 		}
 	}
 
-	private static boolean isPrime(int number) {
-		for (int i = 2; i < number / 2; i++) {
-			if (number % i == 0)
+	static boolean isPrime(int num) {
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
 				return false;
 		}
 		return true;
