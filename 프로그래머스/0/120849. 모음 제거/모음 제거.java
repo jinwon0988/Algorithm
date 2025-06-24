f40d2d9e0bd61dd8f097dbf6f7f2465bf9a55ba9
Jinwon Kim <jinwon0988@naver.com>
2025-06-24 12:41:21 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        my_string = my_string.replace("a", "");
-        my_string = my_string.replace("e", "");
-        my_string = my_string.replace("i", "");
-        my_string = my_string.replace("o", "");
-        my_string = my_string.replace("u", "");
+        my_string = my_string.replaceAll("[aeiou]", "");
         return my_string;
     }
 }
