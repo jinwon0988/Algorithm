663242f2957f45c6a0cd2605d183c0649c5442ce
Jinwon Kim <jinwon0988@naver.com>
2025-06-30 12:00:03 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        for(int i=0; i<s1.length; i++) {
+            for(int j=0; j<s2.length; j++) {
+                if(s1[i].equals(s2[j])) {
+                    answer++;
+                }
+                if(s1[i].charAt(0) < s2[j].charAt(0)) break;
+            }
+        }
+        return answer;
+    }
+}
