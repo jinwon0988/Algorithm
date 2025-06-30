2db20353e4ca63b4a27bbc020f6ad6ad28d7fe25
Jinwon Kim <jinwon0988@naver.com>
2025-07-01 08:14:49 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num, int k) {
+        String s = String.valueOf(num);
+        int answer = s.indexOf(String.valueOf(k));
+        if(answer != -1) answer++;
+        return answer;
+    }
+}
