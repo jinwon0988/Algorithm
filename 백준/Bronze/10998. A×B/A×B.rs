6f8d0c2bc611dfe72c6725b48e6bcc5dbb50a63e
Jinwon Kim <jinwon0988@naver.com>
2025-07-01 21:41:05 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let nums: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    println!("{}", nums[0] * nums[1]);
+}
