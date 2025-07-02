59f423e0e75e486808f292ef7fce5c6ef0dba907
Jinwon Kim <jinwon0988@naver.com>
2025-07-02 09:12:01 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.49 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,10 +2,7 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        char[] A = my_string.toCharArray();
-        for(int i=0; i<A.length; i++) {
-            if(A[i] >= 'A' && A[i] <= 'Z') A[i] = (char)(A[i] - 'A' + 'a');
-        }
+        char[] A = my_string.toLowerCase().toCharArray();
         Arrays.sort(A);
         String answer = String.valueOf(A);
         return answer;
