2c0b08b077d9d79031ac336e9bfa47e5d6dca129
Jinwon Kim <jinwon0988@naver.com>
2025-07-02 09:52:43 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 112 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i <= j)
+					bw.write("*");
+				else
+					bw.write(" ");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
