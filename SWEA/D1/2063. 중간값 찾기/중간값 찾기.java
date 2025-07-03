baf67bab8cc0a5aecb5c3dc9acb3a90f8971219d
Jinwon Kim <jinwon0988@naver.com>
2025-07-03 13:10:39 +0900

    [D1] Title: 중간값 찾기, Time: 128 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] A = new int[T];
-		for (int test_case = 1; test_case <= T; test_case++) {
-			A[test_case - 1] = sc.nextInt();
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
 		}
 		Arrays.sort(A);
-		System.out.println(A[T / 2]);
+		System.out.println(A[N / 2]);
 		sc.close();
 	}
 }
