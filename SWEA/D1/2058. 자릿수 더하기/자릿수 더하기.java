6b3596818928aeecf0154e4b4007bc6aa98ac469
Jinwon Kim <jinwon0988@naver.com>
2025-07-03 13:13:15 +0900

    [D1] Title: 자릿수 더하기, Time: 91 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int N = sc.nextInt();
 		int sum = 0;
-		for (int i = 0; i < 4; i++) {
-			sum += T % 10;
-			T /= 10;
+		while (N > 0) {
+			sum += N % 10;
+			N /= 10;
 		}
 		System.out.println(sum);
 		sc.close();
