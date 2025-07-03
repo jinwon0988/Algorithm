30d1043fa5a4ebf3ee20f470ed064e486a3000a1
Jinwon Kim <jinwon0988@naver.com>
2025-07-03 10:13:35 +0900

    [D1] Title: 신문 헤드라인, Time: 90 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next().toUpperCase();
+		System.out.println(input);
+		sc.close();
+	}
+}
