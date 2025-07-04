d120d07d72a8724984c635846fa9fc5e8d474cc6
Jinwon Kim <jinwon0988@naver.com>
2025-07-04 11:00:01 +0900

    [D1] Title: 스탬프 찍기, Time: 97 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++)
+			System.out.print("#");
+		sc.close();
+	}
+}
