938574ba7d8dee8c0fc304125e4ce813687d54ce
Jinwon Kim <jinwon0988@naver.com>
2025-07-09 23:05:28 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.20 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(String number) {
         int answer = 0;
-        while(number.length() > 1) {
-            int num = Integer.parseInt(number.substring(0,2));
-            num %= 9;
-            number = String.valueOf(num) + number.substring(2);
+        for(int i=0; i<number.length(); i++) {
+            answer += number.charAt(i) - '0';
         }
-        if(number.charAt(0) == '9') answer = 0;
-        else answer = Integer.parseInt(number);
-        return answer;
+        return answer%9;
     }
 }
