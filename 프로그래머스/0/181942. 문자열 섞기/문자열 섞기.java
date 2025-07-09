8aa7eed1296f6166e453b2a51c8db2d1ff39f058
Jinwon Kim <jinwon0988@naver.com>
2025-07-09 12:45:45 +0900

    [level 0] Title: 문자열 섞기, Time: 1.83 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        int a=0, b=0;
-        for(int i=0; i < str1.length() + str2.length(); i++) {
-            if(i%2 == 0) {
-                answer += str1.substring(a, a+1);
-                a++;
-            }else {
-                answer += str2.substring(b, b+1);
-                b++;
-            }
+        for(int i=0; i < str1.length(); i++) {
+            answer += str1.substring(i, i+1);
+            answer += str2.substring(i, i+1);
         }
         return answer;
     }
