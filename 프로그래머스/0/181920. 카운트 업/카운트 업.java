edb7f240477b71808606a7eca78bdedc287e8fd5
Jinwon Kim <jinwon0988@naver.com>
2025-07-11 17:44:26 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num-start_num+1];
+        for(int i=start_num, j=0; i<=end_num; i++) {
+            answer[j++] = i;
+        }
+        return answer;
+    }
+}
