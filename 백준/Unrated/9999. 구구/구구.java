995add5465ae1af3242dbf5e8a31c7e8c4af2d11
Jinwon Kim <jinwon0988@naver.com>
2025-07-19 13:44:30 +0900

    [Unrated] Title: 구구, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Unrated/9999. 구구/구구.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("Hymn To Love\n");
+		bw.close();
+		br.close();
+	}
+}
