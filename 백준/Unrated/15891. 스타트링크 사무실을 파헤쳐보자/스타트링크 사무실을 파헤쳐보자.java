fa2802c45f35fe0969f30948d60a5673b1644b85
Jinwon Kim <jinwon0988@naver.com>
2025-07-19 16:45:28 +0900

    [Unrated] Title: 스타트링크 사무실을 파헤쳐보자, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Unrated/15891. 스타트링크 사무실을 파헤쳐보자/스타트링크 사무실을 파헤쳐보자.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] A = new int[] { 65, 17, 4, 4, 64 };
+		System.out.println(A[sc.nextInt() - 1]);
+		sc.close();
+	}
+}
