5f4f3a5a27bd898a92a5ea99eef50fdc25a67dba
Jinwon Kim <jinwon0988@naver.com>
2025-07-27 10:58:50 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.48 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[5];
         Arrays.sort(num_list);
-        for(int i=0; i<5; i++) answer[i] = num_list[i];
+        answer = Arrays.copyOfRange(num_list, 0, 5);
         return answer;
     }
 }
