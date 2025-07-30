e395c9dca9555094e51d6d3a351c45e6739902e0
Jinwon Kim <jinwon0988@naver.com>
2025-07-30 20:22:05 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 120 ms, Memory: 15564 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		bw.write(S.substring(N - 5, N) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
