695891f4b9103a1b901257063025a66412b70220
Jinwon Kim <jinwon0988@naver.com>
2025-08-22 22:03:25 +0900

    [Gold IV] Title: 최단경로, Time: 684 ms, Memory: 108888 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static ArrayList<Edge>[] A;
+	static int[] distance;
+	static boolean[] visited;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -9,40 +13,23 @@ public class Main {
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(br.readLine());
-		int[] distance = new int[V + 1];
+		A = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
+			A[i] = new ArrayList<>();
+		}
+		distance = new int[V + 1];
+		visited = new boolean[V + 1];
 		for (int i = 0; i <= V; i++) {
 			distance[i] = Integer.MAX_VALUE;
 		}
-		boolean[] visited = new boolean[V + 1];
-		ArrayList<Edge>[] edgeList = new ArrayList[V + 1];
-		for (int i = 0; i <= V; i++) {
-			edgeList[i] = new ArrayList<>();
-		}
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			edgeList[u].add(new Edge(v, w));
+			A[u].add(new Edge(v, w));
 		}
-		pq.add(new Edge(K, 0));
-		distance[K] = 0;
-		while (!pq.isEmpty()) {
-			Edge current = pq.poll();
-			int c_v = current.vertex;
-			if (visited[c_v])
-				continue;
-			visited[c_v] = true;
-			for (Edge tmp : edgeList[c_v]) {
-				int next = tmp.vertex;
-				int value = tmp.value;
-				if (distance[next] > distance[c_v] + value) {
-					distance[next] = distance[c_v] + value;
-					pq.add(new Edge(next, distance[next]));
-				}
-			}
-		}
+		dijkstra(K);
 		for (int i = 1; i <= V; i++) {
 			if (visited[i])
 				bw.write(distance[i] + "\n");
@@ -51,21 +38,42 @@ public class Main {
 		}
 		bw.flush();
 		bw.close();
-	}
-}
-
-class Edge implements Comparable<Edge> {
-	int vertex, value;
-
-	Edge(int vertex, int value) {
-		this.vertex = vertex;
-		this.value = value;
+		br.close();
 	}
 
-	public int compareTo(Edge e) {
-		if (this.value > e.value)
-			return 1;
-		else
-			return -1;
+	public static void dijkstra(int K) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		distance[K] = 0;
+		pq.offer(new Edge(K, 0));
+		while (!pq.isEmpty()) {
+			Edge now = pq.poll();
+			int v = now.v;
+			if (visited[v])
+				continue;
+			visited[v] = true;
+			for (Edge e : A[v]) {
+				int nv = e.v;
+				int nw = e.w;
+				if (distance[nv] > distance[v] + nw) {
+					distance[nv] = distance[v] + nw;
+					pq.add(new Edge(nv, distance[nv]));
+				}
+			}
+		}
+	}
+
+	static class Edge implements Comparable<Edge> {
+		int v;
+		int w;
+
+		Edge(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
+		}
 	}
 }
