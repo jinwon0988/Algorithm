531dbe1a6b0d36eef869fc9424fc84ea0d7c23b7
Jinwon Kim <jinwon0988@naver.com>
2025-08-23 21:02:14 +0900

    [Bronze V] Title: 웰컴, Time: 100 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(".  .   .\n");
+        bw.write("|  | _ | _. _ ._ _  _\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.\n");
+        bw.flush();
+    }
+}
