71d7b612196df6a2f6436b54f4e73569ca280766
Jinwon Kim <jinwon0988@naver.com>
2025-08-30 23:01:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|");
+        System.out.print(sb.toString());
+    }
+}
