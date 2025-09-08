991be1c7c782fa7b75054fb0f972c5239d0dcb51
Jinwon Kim <jinwon0988@naver.com>
2025-09-08 23:20:37 +0900

    [Gold IV] Title: 사이클 게임, Time: 664 ms, Memory: 134452 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] parent;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		parent = new int[n];
+		for (int i = 0; i < n; i++)
+			parent[i] = i;
+		int result = 0;
+		for (int i = 1; i <= m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (find(a) == find(b)) {
+				result = i;
+				break;
+			}
+			union(a, b);
+		}
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b)
+			parent[b] = a;
+	}
+
+	static int find(int a) {
+		if (a == parent[a])
+			return a;
+		else
+			return parent[a] = find(parent[a]);
+
+	}
+}
