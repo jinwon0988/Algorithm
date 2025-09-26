7433aa1296dcbe9a91ef8a381d6ddc6b1c4c52fc
Jinwon Kim <jinwon0988@naver.com>
2025-09-26 23:27:38 +0900

    [Gold V] Title: 최소비용 구하기, Time: 484 ms, Memory: 51584 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,71 +2,75 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static ArrayList<Node>[] edgeList;
+	static ArrayList<Edge>[] A;
 	static int[] distance;
 	static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		edgeList = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			edgeList[i] = new ArrayList<>();
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		A = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = new ArrayList<>();
 		}
 		distance = new int[N + 1];
-		Arrays.fill(distance, Integer.MAX_VALUE);
+		for (int i = 1; i <= N; i++) {
+			distance[i] = Integer.MAX_VALUE;
+		}
 		visited = new boolean[N + 1];
-		StringTokenizer st;
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			edgeList[start].add(new Node(end, weight));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			A[u].add(new Edge(v, w));
 		}
 		st = new StringTokenizer(br.readLine());
-		int S = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int result = dijkstra(S, E);
-		bw.write(result + "\n");
+		int s = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		dijkstra(s);
+		bw.write(distance[e] + "\n");
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 
-	public static int dijkstra(int start, int end) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start, 0));
-		distance[start] = 0;
+	public static void dijkstra(int s) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		distance[s] = 0;
+		pq.offer(new Edge(s, 0));
 		while (!pq.isEmpty()) {
-			Node nowNode = pq.poll();
-			if (!visited[nowNode.targetNode]) {
-				visited[nowNode.targetNode] = true;
-				for (Node n : edgeList[nowNode.targetNode]) {
-					if (distance[n.targetNode] > distance[nowNode.targetNode] + n.value) {
-						distance[n.targetNode] = distance[nowNode.targetNode] + n.value;
-						pq.add(new Node(n.targetNode, distance[n.targetNode]));
-					}
+			Edge now = pq.poll();
+			int v = now.v;
+			if (visited[v])
+				continue;
+			visited[v] = true;
+			for (Edge e : A[v]) {
+				int nv = e.v;
+				int nw = e.w;
+				if (distance[nv] > distance[v] + nw) {
+					distance[nv] = distance[v] + nw;
+					pq.offer(new Edge(nv, distance[nv]));
 				}
 			}
 		}
-		return distance[end];
 	}
 
-	public static class Node implements Comparable<Node> {
-		int targetNode;
-		int value;
+	static class Edge implements Comparable<Edge> {
+		int v;
+		int w;
 
-		Node(int targetNode, int value) {
-			this.targetNode = targetNode;
-			this.value = value;
+		Edge(int v, int w) {
+			this.v = v;
+			this.w = w;
 		}
 
 		@Override
-		public int compareTo(Node o) {
-			return this.value - o.value;
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
 		}
 	}
 }
